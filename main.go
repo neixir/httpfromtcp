9e0144ac4301763a47b5bd14d163e0400909a84b
neixir <neixir@gmail.com>
2025-07-13 19:35:45 +0200

    CH1 L4 Newlines

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -15,6 +16,7 @@ func main() {
 	}
 
 	// Llegim de 8 en 8
+	currentLine := ""
 	for {
 		buf := make([]byte, 8)
 		_, err := f.Read(buf)
@@ -23,7 +25,24 @@ func main() {
 			break
 		}
 
-		fmt.Printf("read: %s\n", buf)
+		// Ajuntem les parts
+		parts := strings.Split(string(buf), "\n")
+
+		// Note that if we only have one "part", we don't need to print, as we have not reached a new line yet.
+		if len(parts) == 1 {
+			currentLine = fmt.Sprintf("%s%s", currentLine, parts[0])
+		} else {
+			for i := 0; i < len(parts)-1; i++ {
+				currentLine = fmt.Sprintf("%s%s", currentLine, parts[i])
+				fmt.Printf("read: %s\n", currentLine)
+				currentLine = ""
+			}
+
+			// Add the last "part" to the "current line" variable. Repeat until you reach the end of the file.
+			currentLine = fmt.Sprintf("%s%s", currentLine, parts[len(parts)-1])
+		}
 	}
+
+	fmt.Printf("read: %s\n", currentLine)
 
 }
