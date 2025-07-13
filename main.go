f6e9a1f05d373d8998783cf5b2aae01beeb6d53f
neixir <neixir@gmail.com>
2025-07-13 17:53:44 +0200

    CH1 L3 8 Bytes

--- main.go
+++ main.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 )
 
 func main() {
-	fmt.Printf("I hope I get the job!")
+	// Obrim el fitxer
+	f, err := os.Open("messages.txt")
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	// Llegim de 8 en 8
+	for {
+		buf := make([]byte, 8)
+		_, err := f.Read(buf)
+
+		if err == io.EOF {
+			break
+		}
+
+		fmt.Printf("read: %s\n", buf)
+	}
+
 }
