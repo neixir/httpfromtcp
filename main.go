6971c8edbb6c43caa9a95a96c1eecf6544171921
neixir <neixir@gmail.com>
2025-07-14 08:27:25 +0200

    CH1 L5 Channel Refactor

--- main.go
+++ main.go
@@ -15,34 +15,51 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Llegim de 8 en 8
-	currentLine := ""
-	for {
-		buf := make([]byte, 8)
-		_, err := f.Read(buf)
-
-		if err == io.EOF {
-			break
-		}
-
-		// Ajuntem les parts
-		parts := strings.Split(string(buf), "\n")
-
-		// Note that if we only have one "part", we don't need to print, as we have not reached a new line yet.
-		if len(parts) == 1 {
-			currentLine = fmt.Sprintf("%s%s", currentLine, parts[0])
-		} else {
-			for i := 0; i < len(parts)-1; i++ {
-				currentLine = fmt.Sprintf("%s%s", currentLine, parts[i])
-				fmt.Printf("read: %s\n", currentLine)
-				currentLine = ""
-			}
-
-			// Add the last "part" to the "current line" variable. Repeat until you reach the end of the file.
-			currentLine = fmt.Sprintf("%s%s", currentLine, parts[len(parts)-1])
-		}
+	for line := range getLinesChannel(f) {
+		fmt.Printf("read: %s\n", line)
 	}
 
-	fmt.Printf("read: %s\n", currentLine)
+}
 
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	line := make(chan string)
+
+	// Llegim de 8 en 8
+	currentLine := ""
+	go func() {
+		defer f.Close()
+		defer close(line)
+		for {
+			buf := make([]byte, 8)
+			_, err := f.Read(buf)
+
+			if err == io.EOF {
+				break
+			}
+
+			// Ajuntem les parts
+			parts := strings.Split(string(buf), "\n")
+
+			// Note that if we only have one "part", we don't need to print, as we have not reached a new line yet.
+			if len(parts) == 1 {
+				currentLine = fmt.Sprintf("%s%s", currentLine, parts[0])
+			} else {
+				for i := 0; i < len(parts)-1; i++ {
+					currentLine = fmt.Sprintf("%s%s", currentLine, parts[i])
+					// fmt.Printf("read: %s\n", currentLine)
+					line <- currentLine
+					currentLine = ""
+				}
+
+				// Add the last "part" to the "current line" variable. Repeat until you reach the end of the file.
+				currentLine = fmt.Sprintf("%s%s", currentLine, parts[len(parts)-1])
+			}
+		}
+
+		// Once you're done reading the file, if there's anything left in the "current line" variable, print it in the same read: LINE format.
+		// fmt.Printf("read: %s\n", currentLine)
+		line <- currentLine
+	}()
+
+	return line
 }
