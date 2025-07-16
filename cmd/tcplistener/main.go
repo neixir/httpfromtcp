d7385e0cdefd096248d531f96eca9c9ad065ab24
neixir <neixir@gmail.com>
2025-07-16 17:45:17 +0200

    CH4 L6 Connect the Parsing

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net"
 	"os"
-	"strings"
+
+	"github.com/neixir/httpfromtcp/internal/request"
 )
 
 func main() {
@@ -28,60 +28,16 @@ func main() {
 			os.Exit(1)
 		}
 
-		// Prints a message to the console that a connection has been accepted.
-		fmt.Println("Connection accepted")
-
-		// Uses our getLinesChannel function to read lines from the connection.
-		for line := range getLinesChannel(conn) {
-			// Prints each line to the console with no additional formatting (aside from a terminating newline).
-			fmt.Println(line)
+		r, err := request.RequestFromReader(conn)
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
 		}
 
-		// Prints a message to the console that the connection has been closed when the channel is closed.
-		fmt.Println("Connection closed")
+		fmt.Println("Request line:")
+		fmt.Printf("- Method: %s\n", r.RequestLine.Method)
+		fmt.Printf("- Target: %s\n", r.RequestLine.RequestTarget)
+		fmt.Printf("- Version: %s\n", r.RequestLine.HttpVersion)
+
 	}
-
-}
-
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	line := make(chan string)
-
-	// Llegim de 8 en 8
-	currentLine := ""
-	go func() {
-		defer f.Close()
-		defer close(line)
-		for {
-			buf := make([]byte, 8)
-			_, err := f.Read(buf)
-
-			if err == io.EOF {
-				break
-			}
-
-			// Ajuntem les parts
-			parts := strings.Split(string(buf), "\n")
-
-			// Note that if we only have one "part", we don't need to print, as we have not reached a new line yet.
-			if len(parts) == 1 {
-				currentLine = fmt.Sprintf("%s%s", currentLine, parts[0])
-			} else {
-				for i := 0; i < len(parts)-1; i++ {
-					currentLine = fmt.Sprintf("%s%s", currentLine, parts[i])
-					// fmt.Printf("read: %s\n", currentLine)
-					line <- currentLine
-					currentLine = ""
-				}
-
-				// Add the last "part" to the "current line" variable. Repeat until you reach the end of the file.
-				currentLine = fmt.Sprintf("%s%s", currentLine, parts[len(parts)-1])
-			}
-		}
-
-		// Once you're done reading the file, if there's anything left in the "current line" variable, print it in the same read: LINE format.
-		// fmt.Printf("read: %s\n", currentLine)
-		line <- currentLine
-	}()
-
-	return line
 }
