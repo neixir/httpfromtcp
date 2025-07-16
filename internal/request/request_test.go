5abe03571bfdb4856e7a41a798d2bd817815c878
neixir <neixir@gmail.com>
2025-07-16 11:09:54 +0200

    CH4 L2 Parsing the Request Line (more tests)

--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -29,4 +29,21 @@ func TestRequestLineParse(t *testing.T) {
 	_, err = RequestFromReader(strings.NewReader("/coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
 	require.Error(t, err)
 
+	// Add more test cases to request_test.go to cover any edge cases you can think of. Here are the names of all the tests I wrote:
+
+	// Good POST Request with path
+	r, err = RequestFromReader(strings.NewReader("POST /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "POST", r.RequestLine.Method)
+	assert.Equal(t, "/coffee", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+
+	// Invalid method (out of order) Request line
+	// Potser permetre nomes GET, POST, PUT, DELETE, UPDATE...
+
+	// Invalid version in Request line
+	_, err = RequestFromReader(strings.NewReader("GET / HTTP/2.0\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)
+
 }
