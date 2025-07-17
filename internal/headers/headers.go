840e900d249b79973aab6749446f714b650c4e2e
neixir <neixir@gmail.com>
2025-07-17 06:18:07 +0200

    CH5 L1 Headers

---
+++ internal/headers/headers.go
@@ -0,0 +1,65 @@
+package headers
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Headers map[string]string
+
+// Mutate the Headers by adding newly parsed key-value pairs
+// Return n (the number of bytes consumed), done (whether or not it has finished parsing headers), and err (if it encountered an error)
+func (h Headers) Parse(data []byte) (n int, done bool, err error) {
+	// Look for a CRLF, if it doesn't find one, assume you haven't been given enough data yet.
+	// Consume no data, return false for done, and nil for err.
+	if !strings.Contains(string(data), "\r\n") {
+		return 0, false, nil
+	}
+
+	// If you do find a CRLF, but it's at the start of the data, you've found the end of the headers,
+	// so return the proper values immediately.
+	parts := strings.Split(string(data), "\r\n")
+	if parts[0] == "" {
+		return 2, true, nil // n=2 per CRLF
+	}
+
+	// En aquest punt ja tenim una linia sencera
+	fieldLine := parts[0]
+
+	// Busquem el primer ":" per dividir
+	i := strings.Index(fieldLine, ":")
+	if i < 0 {
+		return 0, false, fmt.Errorf("malformed header line")
+	}
+
+	key := fieldLine[:i]     // field-name
+	value := fieldLine[i+1:] // field-value
+
+	// L'ultim caracter de la primera part (la clau) no pot ser espai
+	// ("ensure there are no spaces between the colon and the key")
+	lastChar := key[len(key)-1:]
+	if lastChar == " " {
+		return 0, false, fmt.Errorf("malformed header line")
+	}
+
+	// Assuming the format was valid (if it isn't return an error),
+	// add the key/value pair to the Headers map and return the number of bytes consumed.
+	// Note: The Parse function should only return done=true when the data starts with a CRLF,
+	// which can't happen when it finds a new key/value pair.
+	// (pero abans: Remove any extra whitespace from the key and value)
+	key = strings.TrimSpace(key)
+	value = strings.TrimSpace(value)
+	h[key] = value
+	consumed := len(fieldLine) + 2 // per CRLF
+
+	// It's important to understand that this function will be called over and over
+	// until all the headers are parsed, and it can only parse one key/value pair at a time.
+
+	return consumed, false, nil
+}
+
+// Aquesta funcio s'utilitza als test pero no explica com ha de ser. A veure...
+func NewHeaders() Headers {
+	return make(Headers)
+	// return make([]Headers, 10)
+}
---
+++ internal/headers/headers_test.go
@@ -0,0 +1,63 @@
+package headers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHeadersParse(t *testing.T) {
+	// Test: Valid single header
+	headers := NewHeaders()
+	data := []byte("Host: localhost:42069\r\n\r\n")
+	n, done, err := headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, 23, n)
+	assert.False(t, done)
+
+	// Test: Invalid spacing header
+	headers = NewHeaders()
+	data = []byte("       Host : localhost:42069       \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
+	// Add more tests
+
+	// "Valid single header with extra whitespace"
+	headers = NewHeaders()
+	data = []byte("       Host: localhost:42069       \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, 37, n)
+	assert.False(t, done)
+
+	// "Valid 2 headers with existing headers"
+	// Test that when you call Parse multiple times, it adds new headers to the existing map without overwriting previous ones.
+	headers = NewHeaders()
+	data1 := []byte("Host: localhost:42069\r\n")
+	headers.Parse(data1)
+	data2 := []byte("Accept: */*\r\n\r\n")
+	_, done, err = headers.Parse(data2)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, "*/*", headers["Accept"])
+	assert.False(t, done)
+
+	// "Valid done"
+	headers = NewHeaders()
+	data = []byte("\r\n\r\nHola bona tarda")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, 2, n)
+	assert.True(t, done)
+
+}
